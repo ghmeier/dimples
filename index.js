06e838a10d6d86705dfc53b7b75ff2124f7748c7
garret.meier <garret.meier@bunchball.com>
2015-08-07 19:04:33 -0700

    one of those bad errors

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', function(request, response) {
 			var img = JSON.parse(res.body).data;
 			console.log("URL TO POST:"+"https:/slack.com/api/chat.postMessage");
 			console.log("PAYLOAD: "+"payload="+JSON.stringify({channel:channel,username:"Dimplebot",unfurl_links:true,token:token,text:"<"+img.url+">"}));
-			makereq.post("https:/slack.com/api/chat.postMessage",
+			makereq.post("https://slack.com/api/chat.postMessage",
 				JSON.stringify({channel:channel,username:"Dimplebot",unfurl_links:true,token:token,text:"<"+img.url+">"}),
 				function(error,slackRes,body){
 					console.log(error,slackRes,body);
