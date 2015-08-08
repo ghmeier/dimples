d46f5598893891d73898afba0e8f36a9c0fa5b50
garret.meier <garret.meier@bunchball.com>
2015-08-07 18:14:15 -0700

    i can haz a meme

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var makereq = require('request');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,7 +11,23 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+
+	var text = request.query.text;
+	var words = text.split(" ");
+	var top = words.slice(0,words.length/2);
+	var bottom = words.slice(words.length/2);
+
+	makereq("http://api.imgflip.com/caption_image?template_id=42404825&username=headin_thecloud&password=headin_thecloud&text0="+top.join(" ")+"&text1="+bottom.join(" "),
+			function(error,res,body){
+		
+		if (!error && response.statusCode == 200){
+			var img = JSON.parse(res.body).data;
+
+			response.json(img);
+		}
+	});
+
+//	response.json(request.query);
 });
 
 app.listen(app.get('port'), function() {
