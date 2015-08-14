d743324c53fb9218d9619aa1be72a914bb54554b
garret.meier <garret.meier@bunchball.com>
2015-08-14 13:27:35 -0700

    added /random and /list

--- index.js
+++ index.js
@@ -12,6 +12,32 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.get("/list",function(request,response){
+	var images = fb_ref;
+	var imgs = [];
+	images.once("value",function(snapshot){
+		var data = snapshot.val();
+		for (i in data){
+			imgs.push(data[i]);
+		}
+		response.render("meme",{imgs:imgs});
+	});
+});
+
+app.get("/random",function(request,response){
+	fb_ref.once("value",function(snapshot){
+		var data = snapshot.val();
+		var keys = Object.keys(data);
+		var num = Math.floor(Math.random() * keys.length);
+		
+		var imgs = [];
+		imgs.push(data[keys[num]]);
+
+		response.render("meme",{imgs:imgs});
+		
+	});
+});
+
 app.get('/', function(request, response) {
 
 	var text = request.query.text;
@@ -28,22 +54,21 @@ app.get('/', function(request, response) {
 	var token = "xoxp-2153342770-2579407953-5198644515-1e2e4f";
 	var team_id = request.query.team_id;
 	var channel= request.query.channel_id;
-	console.log(request.query);
+	
+	if (!channel || !team_id){
+		response.json({error:"Slack Data Required for meme creation."});
+	}
+
 	makereq("http://api.imgflip.com/caption_image?template_id=42404825&username=headin_thecloud&password=headin_thecloud&text0="+top+"&text1="+bottom,
 			function(error,res,body){
 		
 		if (!error && response.statusCode == 200){
 			var img = JSON.parse(res.body).data;
-			//console.log("URL TO POST:"+"https://slack.com/api/chat.postMessage?channel="+channel+"&username=Dimplebot&token="+token+"&text=<"+img.url+">");
 			fb_ref.push({url:img.url});
 			makereq.get("https://slack.com/api/chat.postMessage?channel="+channel+"&username=Dimplebot&token="+token+"&text=<"+img.url+">",
 				function(error,slackRes,body){
 					console.log(body);
 				});
-
-			//		response.json(body);
-				
-			//response.send(img.url);
 		}
 	});
 
